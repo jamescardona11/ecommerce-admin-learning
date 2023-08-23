75b81a289ef44a811cba0d8a733e0f5b58e94207
jamescardona11 <65878592+jamescardona11@users.noreply.github.com>
2023-08-22 19:36:24 -0500

    Reusable modal and state management

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,16 +1,25 @@
 'use client'
 
-import { UserButton } from '@clerk/nextjs'
-import { Modal } from '@/components/ui/modal'
+import { useEffect } from 'react'
+import { useStoreModal } from '@/hooks/use-store-modal'
 
 function SetupPage () {
+  const onOpen = useStoreModal((state) => state.onOpen)
+  const isOpen = useStoreModal((state) => state.isOpen)
+
+  useEffect(() => {
+    if (!isOpen) {
+      onOpen()
+    }
+  }, [isOpen, onOpen])
+
   return (
     <div className='p-4'>
-      <Modal
+      {/* <Modal
         title='Modal Title'
         description='Modal Description'
         isOpen={true}
-        onClose={() => {}} > Children </Modal>
+        onClose={() => {}} > Children </Modal> */}
     </div>
   )
 }
