c323e992d89e589df98462152c5eaec39e8351ee
jamescardona11 <65878592+jamescardona11@users.noreply.github.com>
2023-08-26 13:28:47 -0500

    Create models in prisma

--- src/hooks/use-origin.tsx
+++ src/hooks/use-origin.tsx
@@ -1,9 +1,13 @@
+/* eslint-disable @typescript-eslint/strict-boolean-expressions */
+/* eslint-disable @typescript-eslint/prefer-optional-chain */
 import { useEffect, useState } from 'react'
 
 export const useOrigin = () => {
   const [mounted, setMounted] = useState(false)
   const origin =
-    (window?.location.origin).length > 0 ? window.location.origin : ''
+    typeof window !== 'undefined' && window.location.origin
+      ? window.location.origin
+      : ''
 
   useEffect(() => {
     setMounted(true)
